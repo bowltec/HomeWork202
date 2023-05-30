25adf84377d5b341466478c308986e0a9e6988eb
Andrey Blinov <bowltec@bk.ru>
2023-05-30 21:14:41 +0300

    Initial Commit

---
+++ HomeWork202/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork202
+//
+//  Created by Андрей Блинов on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
