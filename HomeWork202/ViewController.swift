dbbca3f4432af49cde361a8b74110698636e77f3
Andrey Blinov <bowltec@bk.ru>
2023-05-30 23:20:23 +0300

    Work with constraint in stack view

--- HomeWork202/ViewController.swift
+++ HomeWork202/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
+enum Light {
+    case red
+    case orange
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var nextLightButton: UIButton!
+    
+    private let lightOf: CGFloat = 0.4
+    private let lightOn: CGFloat = 1
+    private var currentLight: Light = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = lightOf
+        orangeLight.alpha = lightOf
+        greenLight.alpha = lightOf
+        
+        nextLightButton.layer.cornerRadius = 15
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        orangeLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        orangeLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
 
-
+    @IBAction func changeLightButtonTapped(_ sender: UIButton) {
+        if nextLightButton.currentTitle == "START" {
+            nextLightButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = lightOn
+            greenLight.alpha = lightOf
+            currentLight = .orange
+        case .orange:
+            redLight.alpha = lightOf
+            orangeLight.alpha = lightOn
+            currentLight = .green
+        case .green:
+            orangeLight.alpha = lightOf
+            greenLight.alpha = lightOn
+            currentLight = .red
+        }
+        
+    }
+    
 }
 
